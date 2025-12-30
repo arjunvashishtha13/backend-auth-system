1ae5bb77168044c9ee3c81266d48d5df5b40f58d
Arjun Vashishtha <arjunvashishtha13@gmail.com>
2025-12-30 15:55:39 +0530

    rebuild:basic express app with src structure

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const express=require('express');
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend running");
-});
+const app = require("./src/app");
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
